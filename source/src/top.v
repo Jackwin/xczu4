76743d1f1fe4d0ea15ec83e8cb58fb231b2ab0bc
Jackwin <rcwcj@126.com>
2021-07-07 08:53:46 +0800

    update top.v

--- source/src/top.v
+++ source/src/top.v
@@ -1,13 +1,20 @@
-module top ();
+module top (
 
-design_1 design_1_wrapper
-   (
-    clk_200(),
-    sys_arstn(),
-    S_AXIS_1_tdata(),
-    S_AXIS_1_tlast(),
-    S_AXIS_1_tready(),
-    S_AXIS_1_tvalid()
-    );
+);
+wire        clk_200;
+wire        s_axis_tlast;
+wire        s_axis_tvalid;
+wire [15:0] s_axis_tdata;
+wire        s_axis_tready;
+wire        sys_arstn;
+
+design_1 design_1_wrapper (
+    .clk_200(clk_200),
+    .sys_arstn(sys_arstn),
+    .S_AXIS_1_tdata(s_axis_tdata),
+    .S_AXIS_1_tlast(s_axis_tlast),
+    .S_AXIS_1_tready(s_axis_tready),
+    .S_AXIS_1_tvalid(s_axis_tvalid)
+);
 
 endmodule
